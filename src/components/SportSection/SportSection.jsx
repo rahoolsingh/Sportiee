1f4ea23a21d9a255da82ece04dbb2b7544f25b70
Veer Rajpoot <rahulksingh3907@gmail.com>
2023-12-21 01:30:25 +0530

    unsplash image missing env fix

--- src/components/SportSection/SportSection.jsx
+++ src/components/SportSection/SportSection.jsx
@@ -2,28 +2,39 @@ import { useEffect, useState } from "react";
 import TeamCard from "../Card/TeamCard";
 
 function SportSection({ data }) {
+  const clientId = import.meta.env.VITE_ACCESS_KEY;
+  const img = data.game;
+  const [res, setRes] = useState([]);
 
-    const img = data.game;
-    const [res, setRes] = useState([]);
+  useEffect(() => {
+    if (clientId) {
+      fetch(
+        `https://api.unsplash.com/search/photos?query=${img}&client_id=${
+          clientId
+        }`
+      )
+        .then((data) => data.json())
+        .then((dataJ) => {
+          setRes(dataJ.results);
+        })
+        .catch((e) => {
+          console.log("Error :: Unable to fetch unsplash images :: ", e);
+        });
+    }
+  }, [res && res.length]);
 
-      useEffect(() => {
-        fetch(
-            `https://api.unsplash.com/search/photos?query=${img}&client_id=${import.meta.env.VITE_ACCESS_KEY}`
-          ).then((data)=> data.json())
-          .then(dataJ => {
-              setRes(dataJ.results)
-          })
-      }, [res.length]);
-
-  return res.length && (
+  return (
     <div className="my-5 pb-2 rounded-md overflow-hidden shadow-sm">
-      <div className="px-4 py-3 text-white font-bold text-xl bg-[#1e1e1f] rounded-xl flex justify-between align-middle">
+      <div className="px-4 py-3 text-white font-bold text-xl bg-[#1e1e1f] rounded-xl">
         <span>{data.game}</span>
-        <span className="btn text-sm py-1 bg-zinc-800 rounded-md text-white px-2"><i className="fa-solid fa-plus"></i> Add Team</span>
       </div>
       <div className="lg:flex lg:w-full md:flex md:w-full w-fit m-auto gap-5 my-5">
         {data.teams.map((team, index) => (
-          <TeamCard data={team} img={res[index].urls.small} key={team.team_name} />
+          <TeamCard
+            data={team}
+            img={res && res.length && res[index].urls.small}
+            key={team.team_name}
+          />
         ))}
       </div>
     </div>
